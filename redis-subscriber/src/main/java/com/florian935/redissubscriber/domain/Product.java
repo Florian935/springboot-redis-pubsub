13fafc9ce91c46e024b80dbbb8b0f1650a0005bc
Florian <florian.martin63000@gmail.com>
2021-07-13 01:41:21 +0200

    Feat: finished subscriber

---
+++ redis-subscriber/src/main/java/com/florian935/redissubscriber/domain/Product.java
@@ -0,0 +1,22 @@
+package com.florian935.redissubscriber.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@FieldDefaults(level = PRIVATE)
+public class Product {
+
+    int id;
+    String name;
+    int quantity;
+    long price;
+}
